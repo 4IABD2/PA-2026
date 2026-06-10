342e6c400040ec9719a67f1854d0eedb6c43f201
victordalet <victordalet@protonmail.com>
2026-06-10 11:31:30 +0200

    feat(web): Add pybind for client

--- web/client/README.md
+++ web/client/README.md
@@ -12,3 +12,25 @@ make
 ```bash
 ./application
 ```
+
+# Build lib for python
+
+- install pybind11 
+
+```bash
+sudo apt install pybind11-dev
+```
+
+- build lib for python
+```bash
+mkdir build_py
+cd build_py
+cmake .. -G "Unix Makefiles" -Dpybind11_DIR="$(uv run python -m pybind11 --cmakedir)"
+make
+```
+
+# test python lib
+
+```bash
+uv run lanch_request.py
+```
--- web/client/main.cpp
+++ web/client/main.cpp
@@ -6,46 +6,21 @@
 #include <unistd.h>
 #include <vector>
 
+#include "request_client.h"
+
+
 int main()
 {
-    // creating socket
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-    // specifying address
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(8080);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-
-    // sending connection request
-    connect(clientSocket, (struct sockaddr*)&serverAddress,
-            sizeof(serverAddress));
-
-    // sending data
-    const char* message = "0;0;0;0;0;0;0";
-    send(clientSocket, message, strlen(message), 0);
-
-    // wait for a response from the server and print it
-    char buffer[1024];
-    ssize_t received = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-
-
-    // output
-    std::vector<float> output = {};
-    char* token = strtok(buffer, ";");
-    while (token != nullptr)
+    std::vector<float> input = {};
+    for (int i = 0; i < 7; i++)
     {
-        output.push_back(std::stof(token));
-        token = strtok(nullptr, ";");
+        input.push_back(rand() % 10);
     }
+    std::vector<float> output = launchRequest(input);
+
     for (int i = 0; i < output.size(); i++)
     {
         std::cout << output[i] << std::endl;
     }
-
-
-    // closing socket
-    close(clientSocket);
-
     return 0;
 }
